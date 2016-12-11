a99effb2bbed39eb9753ae6e8319e975aca62687
akononov <akononov@mit.edu>
2016-12-11 15:33:22 -0600

    Update LU.c

--- LU.c
+++ LU.c
@@ -7,25 +7,45 @@
 to factor the diagonal blocks of the matrix and it will execute on a single processor using OpenMP for parallelization.
 Assume U is initialized with the value of A. */
 
-
 void LU(float* L, float* U, int N) {
-	
-	int i,j,k;
+	// A, L, U are NxN
+	// assume U is initialized with a copy of A
 	
 	// iterate over columns to be eliminated
+	int i,j,k;
 	for (k=0; k<N; k++) {
-		L[k*N+k]=1;
+		L[k*N+k]=1;		// L[k,k]=1
 		
 		// eliminate column k in rows i>k
 		#pragma omp parallel for
 		for (i=k+1; i<N; i++) {
-			L[i*N+k] = U[i*N+k]/U[k*N+k];
+			L[i*N+k] = U[i*N+k]/U[k*N+k];		// multiplier
 			U[i*N+k] = 0;
 
 			// update row i of U and L
 			for (j=k+1; j<N; j++) {
 				L[i*N+j] = 0;
 				U[i*N+j] -= L[i*N+k] * U[k*N+j];
+			}
+		}
+	}
+}
+
+void inplace_LU(float* A, int N) {
+	// A will be overwritten with L below the diagonal and U above the diagonal
+	// A, L, U are NxN
+	
+	// iterate over columns to be eliminated
+	int i,j,k;
+	for (k=0; k<N; k++) {
+		// eliminate column k in rows i>k
+		#pragma omp parallel for
+		for (i=k+1; i<N; i++) {
+			A[i*N+k] = A[i*N+k]/A[k*N+k];		// multiplier L[i,k]
+
+			// update row i of U
+			for (j=k+1; j<N; j++) {
+				A[i*N+j] -= A[i*N+k] * A[k*N+j];
 			}
 		}
 	}
