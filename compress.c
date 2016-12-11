63131516033857a0816cef9878ad6a834503ed6e
akononov <akononov@mit.edu>
2016-12-11 10:47:12 -0600

    Update compress.c

--- compress.c
+++ compress.c
@@ -5,45 +5,53 @@
 /* These functions convert triangular matrices to and from compressed format. Lower triangular matrices are stored row-wise,
 while upper triangular matrices are stored column-wise.*/
 
-void compress_L(float* L, float* packed, int M, int N) {
-	// L is M x N
-	int i,j,k=0;
-	for (i=0; i<M; i++) {
-		for (j=0; j<fmin(N,i+1); j++) {
-			packed[k]=L[i*N+j];
+void compress_L(float* L, float* compressed, int N) {
+	// L is NxN
+	int i,j,k;
+	#pragma omp parallel for schedule(guided)
+	for (i=0; i<N; i++) {
+		k=i*(i+1)/2;
+		for (j=0; j<i+1; j++) {
+			compressed[k]=L[i*N+j];
 			k++;
 		}
 	}
 }
 
-void compress_U(float* U, float* packed, int M, int N) {
-	// U is MxN
-	int i,j,k=0;
+void compress_U(float* U, float* compressed, int N) {
+	// U is NxN
+	int i,j,k;
+	#pragma omp parallel for schedule(guided)
 	for (j=0; j<N; j++) {
-		for (i=0;i<fmin(j+1,M); i++) {
-			packed[k]=U[i*N+j];
+	  k=j*(j+1)/2;
+		for (i=0; i<j+1; i++) {
+			compressed[k]=U[i*N+j];
 			k++;
 		}
 	}
 }
 
-void uncompress_L(float* packed, float* L, int M, int N) {
-	// L is MxN
-	int i,j,k=0;
-	for (i=0; i<M; i++) {
-		for (j=0; j<fmin(N,i+1); j++) {
-			L[i*N+j]=packed[k];
+void uncompress_L(float* compressed, float* L, int N) {
+	// L is NxN
+	int i,j,k;
+	#pragma omp parallel for schedule(guided)
+	for (i=0; i<N; i++) {
+	  k=i*(i+1)/2;
+		for (j=0; j<i+1; j++) {
+			L[i*N+j]=compressed[k];
 			k++;
 		}
 	}
 }
 
-void uncompress_U(float* packed, float* U, int M, int N) {
-	// U is MxN
-	int i,j,k=0;
+void uncompress_U(float* compressed, float* U, int N) {
+	// U is NxN
+	int i,j,k;
+	#pragma omp parallel for schedule(guided)
 	for (j=0; j<N; j++) {
-		for (i=0; i=fmin(j+1,M); i++) {
-			U[i*N+j]=packed[k];
+	  k=j*(j+1)/2;
+		for (i=0; i<j+1; i++) {
+			U[i*N+j]=compressed[k];
 			k++;
 		}
 	}
