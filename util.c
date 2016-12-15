eead0d1a2e37870cc58798ae7795acb83c29d644
akononov <kononov2@illinois.edu>
2016-12-15 15:55:36 -0600

    debugging

--- util.c
+++ util.c
@@ -10,7 +10,7 @@ void parse_args(int argc, char** argv, size_t* n, size_t* t, size_t* b) {
   // Default number of threads
   *t = 12;
 
-  while ((option = getopt(argc, argv, "n:t:b")) != -1) {
+  while ((option = getopt(argc, argv, "n:t:b:")) != -1) {
     switch (option) {
       case 'n':
         sscanf(optarg, "%zu", n);
@@ -20,8 +20,9 @@ void parse_args(int argc, char** argv, size_t* n, size_t* t, size_t* b) {
         break;
       case 'b':
       	sscanf(optarg, "%zu", b);
+	break;
       default:
-        printf("Usage: %s -n NUM -t NUMTHREADS\n", argv[0]);
+        printf("Usage: %s -n NUM -t NUMTHREADS -b BLOCKSIZE\n", argv[0]);
         exit(EXIT_FAILURE);
     }
   }
