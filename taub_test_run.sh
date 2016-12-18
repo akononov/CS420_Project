cba83b4e7a712126958c27ead8f535b5fb5f1057
akononov <kononov2@illinois.edu>
2016-12-18 06:57:52 -0600

    debugging

--- taub_test_run.sh
+++ taub_test_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PBS -l walltime=00:30:00
+#PBS -l walltime=00:10:00
 #PBS -l nodes=2:ppn=12
 #PBS -N cs420_project
 #PBS -q cse
@@ -15,7 +15,7 @@ MATRIX_SIZE=100
 BLOCK_SIZE=25
 
 echo "Compiling main"
-mpiicc main.c -std=c99 -lrt -qopenmp -o main -g -O0
+mpiicc main.c -std=c99 -lrt -qopenmp -D_POSIX_C_SOURCE=199309L -o main #-g -O0
 
 echo "Running main"
 #mpirun -np ${NUM_RANKS} -ppn 1 valgrind -v --leak-check=yes ./main -t 12 -n ${MATRIX_SIZE} -b ${BLOCK_SIZE}
